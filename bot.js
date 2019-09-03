009c88b0516138432a048ae5a7a125ed6813f25f
anasshadow <54816572+anasshadow@users.noreply.github.com>
2019-09-03 15:59:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+const client3 = new Discord.Client();
 console.log("Scrpit By Shadow");
 const myid = ['524218118462242816'];
 const prefix = ['!'];
